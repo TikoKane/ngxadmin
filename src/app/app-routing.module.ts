d538ec6d3195eeb2e6a739076ce9f17237187db6
TikoKane <tikokane11@gmail.com>
2021-04-29 15:54:33 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import {
+  NbAuthComponent,
+  NbLoginComponent,
+  NbLogoutComponent,
+  NbRegisterComponent,
+  NbRequestPasswordComponent,
+  NbResetPasswordComponent,
+} from '@nebular/auth';
+import { LoginComponent } from './auth/login/login.component';
+import { ChangerpasswordComponent } from './auth/changerpassword/changerpassword.component';
+
+export const routes: Routes = [
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module')
+      .then(m => m.PagesModule),
+  },
+  {
+    path: 'auth',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'changemdp',
+        component: ChangerpasswordComponent,
+      },
+     
+    ],
+  },
+];
+
+const config: ExtraOptions = {
+  useHash: false,
+};
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, config)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {
+}
